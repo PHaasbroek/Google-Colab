b411fa73b3df63aebd2224b97b0cac53b427e948
PHaasbroek <85097155+PHaasbroek@users.noreply.github.com>
2024-02-18 11:19:02 +0200

    Update functions.py
    Added the speed of sound in gas function to the functions module

--- functions.py
+++ functions.py
@@ -1,1 +1,8 @@
-functionsTest = 69
+def c_sound_f(T, k, R):
+  c_square = T * k * R
+
+  if c_square < 0:
+    print("Speed of sound must be a positive value!")
+    return 0
+
+  return (c_square)**(0.5)
