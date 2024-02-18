a1fa42117020c46c99e3f742c30002533f6d71ed
PHaasbroek <85097155+PHaasbroek@users.noreply.github.com>
2024-02-18 11:22:37 +0200

    Update equations.py
    Added:
    1. Description and units of arguments
    2. Description of if-statement

--- equations.py
+++ equations.py
@@ -1,7 +1,11 @@
-def c_sound_f(T, k, R):
-  c_square = T * k * R
+def c_sound(T, k, R):
+  # T is temperature [K]
+  # k is heat capacity ratio [-]. Usually 1.4 for air
+  # R is the specific gas constant. Usually = 287 # [J/kg/K] for air
+  
+  c_square = T * k * R 
 
-  if c_square < 0:
+  if c_square < 0: # test to see if positive
     print("Speed of sound must be a positive value!")
     return 0
 
