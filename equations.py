cf18cd9d8517628215dbfb202a9afe31337d347c
PHaasbroek <85097155+PHaasbroek@users.noreply.github.com>
2024-02-18 21:08:55 +0200

    Update equations.py

--- equations.py
+++ equations.py
@@ -1,10 +1,3 @@
-def density (p, R, T):
-  # T is temperature [K]
-  # p is pressure [Pa]. 
-  # R is the specific gas constant. Usually = 287.0 # [J/kg/K] for air
-  
-  return p / R / T
-
 def c_sound(T, k, R):
   # T is temperature [K]
   # k is heat capacity ratio [-]. Usually 1.4 for air
@@ -17,4 +10,13 @@ def c_sound(T, k, R):
     return 0
 
   return (c_square)**(0.5)
+  
+def density (p, R, T):
+  # T is temperature [K]
+  # p is pressure [Pa]. 
+  # R is the specific gas constant. Usually = 287.0 # [J/kg/K] for air
+  
+  return p / R / T
+
+
 
